df62427e4e3c6ccfa948b8c4de7c169ec64df095
Antonio Victor Ribeiro Morelli <victor.morelli.007@gmail.com>
2021-02-18 13:22:32 -0300

    all validations implemented on the models: cocktail, dose and ingredient

---
+++ app/models/dose.rb
@@ -0,0 +1,8 @@
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+
+  validates :description, :ingredient, :cocktail, presence: true
+
+  validates_uniqueness_of :cocktail_id, scope: :ingredient_id
+end
