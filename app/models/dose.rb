7e98d65ee707534cff736e4ef0340c99a9e0e3e8
Antonio Victor Ribeiro Morelli <victor.morelli.007@gmail.com>
2021-02-18 15:24:24 -0300

    seed working

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,7 +2,7 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
 
-  validates :description, :ingredient, :cocktail, presence: true
+  validates :description, presence: true
 
-  validates_uniqueness_of :cocktail_id, scope: :ingredient_id
+  validates_uniqueness_of :cocktail, scope: :ingredient
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+base_url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+result = JSON.parse(open(base_url).read)
+drinks = result['drinks']
+
+puts 'cleaning up Ingredient database...'
+Ingredient.destroy_all
+puts 'database is clean!'
+
+puts 'Creating ingredients'
+
+10.times do
+  new_drink = drinks.sample
+  ingredient = Ingredient.create(
+    name: new_drink.values.first
+  )
+  puts "ingredient #{ingredient.id} is created."
+end
+
+puts 'All Done!'
