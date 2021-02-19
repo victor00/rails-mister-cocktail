0620ce778476b9b8a21bc78f6e3ac3a5e361a529
Antonio Victor Ribeiro Morelli <victor.morelli.007@gmail.com>
2021-02-19 13:15:59 -0300

    before_validation to cocktail model - method downcase_names

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,5 +2,11 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
 
+  before_validation :downcase_names
+
   validates :name, presence: true, uniqueness: true
+
+  def downcase_names
+    self.name = name.downcase unless name.nil?
+  end
 end
