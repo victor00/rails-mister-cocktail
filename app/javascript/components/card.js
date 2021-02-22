493b7b92df2d48a1c01c105fad9e05423f7729d8
Antonio Victor Ribeiro Morelli <victor.morelli.007@gmail.com>
2021-02-21 21:46:33 -0300

    back to last commit update text of edit doses

--- app/javascript/components/card.js
+++
@@ -1,1 +0,0 @@
-import AOS from 'aos';
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require('card');
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -21,5 +21,4 @@ document.addEventListener('turbolinks:load', () => {
 	// Call your JS functions here
 	// [...]
 	loadDynamicTitleText();
-	AOS.init();
 });
